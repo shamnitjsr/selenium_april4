6c09af1162640b78f6516bb0a9cc355bf45435e7
sam02 <shamnitjsr@gmail.com>
2019-04-03 09:42:29 -0700

    second commit in april

---
+++ src/com/ibm/sam/Spicejet.java
@@ -0,0 +1,19 @@
+package com.ibm.sam;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Spicejet {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver",
+				 "D:\\selenium_setupfile\\chromedriver_win32\\chromedriver.exe"); WebDriver
+				  driver=new ChromeDriver();
+				 driver.get("https://www.spicejet.com/");
+					driver.manage().window().maximize();
+					driver.close();
+
+	}
+
+}
