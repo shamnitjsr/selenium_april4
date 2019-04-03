e845b48cf7d1bd104e0082c120c596f243840ae4
sam <shamnitjsr@gmail.com>
2019-04-03 10:48:20 -0700

    first commit in april

--- src/com/ibm/sam/Facbook.java
+++ src/com/ibm/sam/Facbook.java
@@ -10,26 +10,24 @@ public class Facbook {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		
-		 System.setProperty("webdriver.chrome.driver",
-		 "D:\\selenium_setupfile\\chromedriver_win32\\chromedriver.exe"); WebDriver
-		  driver=new ChromeDriver();
-		 
+		System.setProperty("webdriver.chrome.driver", "D:\\selenium_setupfile\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 
 		/*
-		System.setProperty("webdriver.gecko.driver",
-				"D:\\selenium_setupfile\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		 * System.setProperty("webdriver.gecko.driver",
+		 * "D:\\selenium_setupfile\\geckodriver-v0.24.0-win64\\geckodriver.exe");
+		 * 
+		 * //geckodriver version 24 and firefox (Version: 64.0-beta-8)
+		 * 
+		 * WebDriver driver = new FirefoxDriver();
+		 */
 
-		//geckodriver version 24 and firefox (Version:	64.0-beta-8)
+		/*
+		 * System.setProperty("webdriver.ie.driver", "D:\\\\MicrosoftWebDriver.exe");
+		 * 
+		 * WebDriver driver = new InternetExplorerDriver();
+		 */
 
-		WebDriver driver = new FirefoxDriver();
-		*/
-
-		/*System.setProperty("webdriver.ie.driver",
-				"D:\\\\MicrosoftWebDriver.exe");
-
-		WebDriver driver = new InternetExplorerDriver();*/
-		
 		driver.get("https://www.facebook.com/");
 		driver.manage().window().maximize();
 		driver.close();
--- src/com/ibm/sam/Spicejet.java
+++ src/com/ibm/sam/Spicejet.java
@@ -7,14 +7,15 @@ public class Spicejet {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.setProperty("webdriver.chrome.driver",
-				 "D:\\selenium_setupfile\\chromedriver_win32\\chromedriver.exe"); WebDriver
-				  driver=new ChromeDriver();
-				 driver.get("https://www.spicejet.com/");
-					driver.manage().window().maximize();
-			driver.navigate().to("https://www.facebook.com/");
+		System.setProperty("webdriver.chrome.driver", "D:\\selenium_setupfile\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.spicejet.com/");
+		driver.manage().window().maximize();
+		driver.navigate().to("https://www.facebook.com/");
+		System.out.println(driver.getCurrentUrl());
 		
-					driver.close();
+
+		driver.close();
 
 	}
 
